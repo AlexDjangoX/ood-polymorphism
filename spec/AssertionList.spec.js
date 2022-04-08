c1cc3c2e899cecd075c3aeceefb5b396e6602b3a
AlexDjangoX <alexmonk17@gmail.com>
2022-04-08 11:49:09 +0200

    done

--- spec/AssertionList.spec.js
+++ spec/AssertionList.spec.js
@@ -1,23 +1,29 @@
-const AssertionList = require("../src/AssertionList.js")
-const Equal = require("../src/Equal.js")
-const Include = require("../src/Include.js")
+const AssertionList = require("../src/AssertionList.js");
+const Equal = require("../src/Equal.js");
+const Include = require("../src/Include.js");
 
-fdescribe("AssertionList", () => {
-  describe("#check()", () => {
-    it("returns true when assertions are true", () => {
-      const equalAssertion = new Equal(1, 1)
-      const includeAssertion = new Include([1,2,3], 3)
-      const assertionList = new AssertionList([equalAssertion, includeAssertion])
+describe("AssertionList", () => {
+    describe("#check()", () => {
+        it("returns true when assertions are true", () => {
+            const equalAssertion = new Equal(1, 1);
+            const includeAssertion = new Include([1, 2, 3], 3);
+            const assertionList = new AssertionList([
+                equalAssertion,
+                includeAssertion
+            ]);
 
-      expect(assertionList.checkAll()).toEqual(true)
-    })
+            expect(assertionList.checkAll()).toEqual(true);
+        });
 
-    it("returns false when at least 1 assertion is false", () => {
-      const equalAssertion = new Equal(1, 2)
-      const includeAssertion = new Include([1,2,3], 3)
-      const assertionList = new AssertionList([equalAssertion, includeAssertion])
+        it("returns false when at least 1 assertion is false", () => {
+            const equalAssertion = new Equal(1, 2);
+            const includeAssertion = new Include([1, 2, 3], 3);
+            const assertionList = new AssertionList([
+                equalAssertion,
+                includeAssertion
+            ]);
 
-      expect(assertionList.checkAll()).toEqual(false)
-    })
-  })
-})
+            expect(assertionList.checkAll()).toEqual(false);
+        });
+    });
+});
--- src/AssertionList.js
+++ src/AssertionList.js
@@ -1,20 +1,22 @@
-const Equal = require("./Equal.js")
-const Include = require("./Include.js")
+const Equal = require("./Equal.js");
+const Include = require("./Include.js");
 
 class AssertionList {
-  constructor(assertions) {
-    this.assertions = assertions
-  }
+    constructor(assertions) {
+        this.assertions = assertions;
+    }
 
-  checkAll() {
-    return this.assertions.every(assertion => {
-      if (assertion instanceof Equal) {
-        return assertion.checkEqual()
-      } else if (assertion instanceof Include) {
-        return assertion.checkInclude()
-      }
-    })
-  }
+    checkAll() {
+        return this.assertions.every((assertion) => {
+            return assertion.equal();
+        });
+    }
 }
 
-module.exports = AssertionList
+module.exports = AssertionList;
+
+// if (assertion instanceof Equal) {
+//   return assertion.checkEqual()
+// } else if (assertion instanceof Include) {
+//   return assertion.checkInclude()
+// }
